925933a755baabeedb033f2fe27a09fb722af557
Christopher Zorn <zorn@psu.edu>
2016-08-23 10:51:54 -0400

    Code

---
+++ Code/DayZero.R
@@ -0,0 +1,52 @@
+##########################################
+# PLSC 502 -- Fall 2016
+#
+# "Day Zero" code...
+#
+# File created 8/23/2016
+#
+# File last modified 8/23/2016
+#
+##########################################
+# This code isn't actually code, really.
+#
+# It's just a placeholder.
+#
+# That said, if you want to do yourself a
+# big favor, learn about "R Task Views:
+#
+# https://cran.r-project.org/web/views/
+#
+# They are a great way to install and/or
+# update R packages. To get started, just do 
+# the following:
+
+install.packages("ctv")
+library(ctv) 
+
+# ...then uncomment the lines of the task views 
+# that you want to install:
+#
+# install.views("Bayesian")
+# install.views("Cluster")
+# install.views("Distributions")
+# install.views("Econometrics")
+# install.views("ExperimentalDesign")
+# install.views("Finance")
+# install.views("Genetics")
+# install.views("Graphics")
+# install.views("MachineLearning")
+# install.views("Multivariate")
+# install.views("NaturalLanguageProcessing")
+# install.views("Optimization")
+# install.views("Psychometrics")
+# install.views("Robust")
+install.views("SocialSciences")
+# install.views("Spatial")
+# install.views("Survival")
+# install.views("TimeSeries")
+# install.views("gR")
+
+# etc.
+
+
